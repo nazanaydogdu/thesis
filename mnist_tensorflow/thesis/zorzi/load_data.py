dbffb2e3582fb8729190a7c74bbcdbc959c3c1d5
Nazan Aydogdu <nazanaydogdu@gmail.com>
2018-10-26 12:49:29 +0300

    reformat code

--- mnist_tensorflow/thesis/zorzi/load_data.py
+++ mnist_tensorflow/thesis/zorzi/load_data.py
@@ -2,51 +2,30 @@ import np_utils as np_utils
 
 from itertools import permutations
 import numpy as np
-import pandas as pd
-
-raw_text = 'ABCDEF'
-raw_input = []
-enum_input = []
 
 
-chars = sorted(list(set(raw_text)))
-char_to_int = dict((c, i) for i, c in enumerate(chars))
-for x in list(permutations(raw_text, 3)):
-    raw_input.append("".join(x)) #120 words like ABC ABD ABF
-    '''enum_input.append([char_to_int[char] for char in "".join(x)])'''
+def load_data():
+    raw_text = 'ABCDEF'
+    raw_input = []
+    alphabet = []
 
-'''np.savetxt("rawvocab.csv", raw_input, fmt="%s", delimiter=",")'''
+    for x in list(permutations(raw_text, 3)):
+        raw_input.append("".join(x))  # 120 words like ABC ABD ABF
 
+    for letter in range(65, 91):
+        alphabet.append(chr(letter))
 
-n_chars = len(raw_text)
-'''enum_input = enum_input / float(n_chars)
-X = np_utils.to_categorical(enum_input)'''
+    matrix = np.array(np.zeros((3, len(alphabet))), dtype=int)
 
-alphabet = []
-for letter in range(65, 91):
-    alphabet.append(chr(letter))
+    for word in raw_input:
+        matrix_new = np.array(np.zeros((3, len(alphabet))), dtype=int)
+        for i in range(3):
+            matrix_new[i, ord(word[i]) - 65] = 1;
+        final_matrix = np.concatenate((matrix, matrix_new), axis=1)
+        matrix = final_matrix;
 
-matrix = np.array(np.zeros((3, len(alphabet))), dtype=int)
-row_names = [_ for _ in sorted(range(3))]
-col_names = [_ for _ in alphabet]
+    matrix = np.delete(matrix, np.s_[0:26], axis=1)
+    np.savetxt("rawdata.csv", matrix, fmt="%d", delimiter=",")
+    return matrix
 
-df = pd.DataFrame(matrix, index=row_names, columns=col_names)
-
-for word in raw_input:
-    #dfTemp = df.copy();
-    matrix_new = np.array(np.zeros((3, len(alphabet))), dtype=int)
-    for i in range(3):
-        #dfTemp.set_value(i, word[i], 1)
-        matrix_new[i, ord(word[i])-65] = 1;
-        #df.set_value(i, word[i], 1)
-    #pd.concat([df, dfTemp], axis=1)
-    #pd.concat([df, df], axis=1)
-    final_matrix = np.concatenate((matrix, matrix_new), axis=1)
-    matrix = final_matrix;
-matrix = np.delete(matrix, np.s_[0:26], axis=1)
-np.savetxt("rawdata.csv", matrix, fmt="%d", delimiter=",")
-
-
-
-print()
-
+print(load_data())
