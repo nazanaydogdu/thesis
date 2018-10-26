35980ab4c4ae8120ff9c04709d1fc0036a44a0c0
Nazan Aydogdu <nazanaydogdu@gmail.com>
2018-10-26 13:40:25 +0300

    load_data() function returns inputdata and targets

--- mnist_tensorflow/thesis/zorzi/load_data.py
+++ mnist_tensorflow/thesis/zorzi/load_data.py
@@ -1,5 +1,3 @@
-import np_utils as np_utils
-
 from itertools import permutations
 import numpy as np
 
@@ -8,6 +6,7 @@ def load_data():
     raw_text = 'ABCDEF'
     raw_input = []
     alphabet = []
+    letter_size = 3
 
     for x in list(permutations(raw_text, 3)):
         raw_input.append("".join(x))  # 120 words like ABC ABD ABF
@@ -15,17 +14,29 @@ def load_data():
     for letter in range(65, 91):
         alphabet.append(chr(letter))
 
-    matrix = np.array(np.zeros((3, len(alphabet))), dtype=int)
+    matrix = np.array(np.zeros((letter_size, len(alphabet))), dtype=int)
+    input_data = np.array(np.zeros((1, letter_size*len(alphabet))), dtype=int)
+    targets = np.array(np.zeros((1, len(raw_input))), dtype=int)
 
     for word in raw_input:
-        matrix_new = np.array(np.zeros((3, len(alphabet))), dtype=int)
-        for i in range(3):
-            matrix_new[i, ord(word[i]) - 65] = 1;
+        matrix_new = np.array(np.zeros((letter_size, len(alphabet))), dtype=int)
+        for i in range(letter_size):
+            matrix_new[i, ord(word[i]) - 65] = 1
+        final_flat_matrix = np.concatenate((input_data, np.ravel(matrix_new).reshape(1,letter_size*len(alphabet))), axis=0)
+        input_data = final_flat_matrix
         final_matrix = np.concatenate((matrix, matrix_new), axis=1)
-        matrix = final_matrix;
+        matrix = final_matrix
 
-    matrix = np.delete(matrix, np.s_[0:26], axis=1)
-    np.savetxt("rawdata.csv", matrix, fmt="%d", delimiter=",")
-    return matrix
+    for i in range(len(raw_input)):
+        target = np.array(np.zeros((1, len(raw_input))), dtype=int)
+        target[0, i] = 1
+        final_targets = np.concatenate((targets, target), axis=0)
+        targets = final_targets
+
+    input_data = np.delete(input_data, (0), axis=0)
+    targets = np.delete(targets, (0), axis=0)
+    matrix = np.delete(matrix, np.s_[0:len(alphabet)], axis=1)
+
+    return input_data, targets
 
 print(load_data())
